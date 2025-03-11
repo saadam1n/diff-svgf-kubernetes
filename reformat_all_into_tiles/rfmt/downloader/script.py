7017833b4aac8dddd72e19f4455fbb9418370fc9
Saad Amin <58188800+saadam1n@users.noreply.github.com>
2025-03-11 15:10:54 -0400

    Add utilties to minify a dataset

--- reformat_all_into_tiles/rfmt/downloader/script.py
+++ reformat_all_into_tiles/rfmt/downloader/script.py
@@ -2,24 +2,36 @@ import sys
 import os
 import shutil
 import random
+import multiprocessing
 
 def estimate_count(src_dir, suffix, prob):
     actual_src = src_dir + suffix
     all_folders = os.listdir(actual_src)
     print(f"Found {len(all_folders)} items in {suffix}. Expecting to copy over approximately {len(all_folders) * prob} items.")
 
+def copy_if_random(args):
+    actual_src, actual_dst, prob, folder = args
+
+    seed = int.from_bytes(os.urandom(4), "big")
+    random.seed(seed)
+
+    if random.random() < prob:
+        src_folder = actual_src + folder
+        print(f"\tCopying {src_folder}")
+        shutil.copyfile(src_folder, actual_dst + folder)
+
 def copy_in_folder(src_dir, dst_dir, suffix, prob):
     actual_src = src_dir + suffix
     actual_dst = dst_dir + suffix
 
     all_folders = os.listdir(actual_src)
+
+    os.makedirs(actual_dst)
     
-    #print(f"Found {len(all_folders)} items in {suffix}. Expecting to copy over approximately {len(all_folders) * prob} items.")
-    for folder in all_folders:
-        if random.random() < prob:
-            src_folder = actual_src + folder
-            print(f"\tCopying {src_folder}")
-            shutil.copytree(src_folder, actual_dst + folder)
+    args = [(actual_src, actual_dst, prob, folder) for folder in all_folders]
+    with multiprocessing.Pool() as p:
+        p.map(copy_if_random, args)
+
 
 
 if __name__ == "__main__":
